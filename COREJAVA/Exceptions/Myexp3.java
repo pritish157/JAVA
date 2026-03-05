5ff45b4542ae32e1f4263a6942616f8cc50a2af5
pritish157 <pritishpanda157@gmail.com>
2026-03-05 15:33:17 +0530

    ..

--- COREJAVA/Exceptions/Myexp3.java
+++ COREJAVA/Exceptions/Myexp3.java
@@ -1,10 +1,25 @@
 package Exceptions;
 
+import java.util.Scanner;
+
 public class Myexp3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("connection establlished.");
+
+		int[] arr = new int[3];
+
+		arr[0] = 10;
+
+		arr[1] = 20;
+		arr[2] = 30;
+		System.out.println("enter the index you want to access: ");
+		int index = sc.nextInt();
+		System.out.println(arr[index]);
+
+		System.out.println("connection terminated..");
 	}
 
 }
