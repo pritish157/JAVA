b9c24677f30916c79325edf2b8a44a0eda74d573
pritish157 <pritishpanda157@gmail.com>
2026-03-05 16:14:55 +0530

    try catch3

--- COREJAVA/Exceptions/MyExp4.java
+++ COREJAVA/Exceptions/MyExp4.java
@@ -7,11 +7,14 @@ public class MyExp4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("connection establlished.");
-		System.out.println("enter the size of an array: ");
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-
-		System.out.println("length of the array: " + arr.length);
+		try {
+			System.out.println("enter the size of an array: ");
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			System.out.println("length of the array: " + arr.length);
+		} catch (Exception e) {
+			System.out.println("array index cant be negative.");
+		}
 
 		System.out.println("connection terminated..");
 	}
--- COREJAVA/Exceptions/Myexp3.java
+++ COREJAVA/Exceptions/Myexp3.java
@@ -8,17 +8,21 @@ public class Myexp3 {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("connection establlished.");
-
 		int[] arr = new int[3];
 
 		arr[0] = 10;
 
 		arr[1] = 20;
 		arr[2] = 30;
-		System.out.println("enter the index you want to access: ");
-		int index = sc.nextInt();
-		System.out.println(arr[index]);
+		try {
 
+			System.out.println("enter the index you want to access: ");
+			int index = sc.nextInt();
+			System.out.println(arr[index]);
+
+		} catch (Exception e) {
+			System.out.println("index must be less than equal to " + arr.length);
+		}
 		System.out.println("connection terminated..");
 	}
 
