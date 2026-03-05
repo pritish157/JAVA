5de29e2600bbbf98bbb46b48056f350af7732563
pritish157 <pritishpanda157@gmail.com>
2026-03-05 15:55:12 +0530

    try catch

--- COREJAVA/Exceptions/MyExp2.java
+++ COREJAVA/Exceptions/MyExp2.java
@@ -6,12 +6,16 @@ public class MyExp2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("connectioon established.");
-		System.out.println("enter int a ");
-		int a = sc.nextInt();
-		System.out.println("enter int b ");
-		int b = sc.nextInt();
-		int c = a / b;
-		System.out.println("result of a/b is " + c);
+		try {
+			System.out.println("enter int a ");
+			int a = sc.nextInt();
+			System.out.println("enter int b ");
+			int b = sc.nextInt();
+			int c = a / b;
+			System.out.println("result of a/b is " + c);
+		} catch (Exception e) {
+			System.out.println("int b cant be 0");
+		}
 		System.out.println("connection terminatd.");
 	}
 }
