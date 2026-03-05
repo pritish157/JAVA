ea8cf1509a1113b96512d1e2d1f60231b1e3e22a
pritish157 <pritishpanda157@gmail.com>
2026-03-05 15:47:16 +0530

    try,catch

--- COREJAVA/Exceptions/MyExp1.java
+++ COREJAVA/Exceptions/MyExp1.java
@@ -7,10 +7,14 @@ public class MyExp1 {
 	public static void main(String[] args) {
 		System.out.println("connections established...");
 		Scanner s = new Scanner(System.in);
-		System.out.println("enter your age:");
-		int age = s.nextInt();
-		System.out.println("age is " + age);
+		try {
+			System.out.println("enter your age:");
+			int age = s.nextInt();
+			System.out.println("age is " + age);
 
+		} catch (Exception e) {
+			System.out.println("Age can't be a string. kindly, provide a integer.");
+		}
 		System.out.println("connection terminated gracefully......");
 	}
 
