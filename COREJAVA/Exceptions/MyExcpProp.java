e33a123779cb92aa8048afa711370248830c5112
pritish157 <pritishpanda157@gmail.com>
2026-03-06 15:12:12 +0530

    .

--- COREJAVA/Exceptions/MyExcpProp.java
+++ COREJAVA/Exceptions/MyExcpProp.java
@@ -1,9 +1,49 @@
 package Exceptions;
 
+import java.util.Scanner;
+
+class Demo3 {
+
+	public void m3() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Connection4 established");
+		System.out.println("Enter your age");
+		int age = scan.nextInt();
+		System.out.println("Age entered is " + age);
+		System.out.println("Connection4 terminated");
+
+	}
+}
+
+class Demo2 {
+
+	public void m2() {
+		System.out.println("Connection3 established");
+		Demo3 d3 = new Demo3();
+		d3.m3();
+		System.out.println("Connection3 terminated");
+
+	}
+}
+
+class Demo1 {
+
+	public void m1() {
+		System.out.println("Connection2 established");
+		Demo2 d2 = new Demo2();
+		d2.m2();
+		System.out.println("Connection2 terminated");
+
+	}
+}
+
 public class MyExcpProp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Connection1 established");
+		Demo1 d1 = new Demo1();
+		d1.m1();
+		System.out.println("Connection1 terminated");
 
 	}
 
