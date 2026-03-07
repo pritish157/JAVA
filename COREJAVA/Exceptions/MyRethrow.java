2570d0518c7e9dc91c031f0f0cb7c1e6a8c18153
pritish157 <pritishpanda157@gmail.com>
2026-03-07 14:49:16 +0530

    ..

---
+++ COREJAVA/Exceptions/MyRethrow.java
@@ -0,0 +1,44 @@
+package Exceptions;
+
+import java.util.Scanner;
+
+class MyATM {
+
+	public void m1() throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Connection established");
+		try {
+			System.out.println("Enter a:");
+			int a = sc.nextInt();
+			System.out.println("Enter b:");
+			int b = sc.nextInt();
+			int c = a / b;
+			System.out.println("Result of a/b is " + c);
+		} catch (Exception e) {
+			System.out.println("Sorry for incovenience. we ll get back soon");
+			throw e;
+		} finally {
+			System.out.println("Connection terminated");
+			sc.close();
+		}
+
+	}
+
+}
+
+public class MyRethrow {
+
+	public static void main(String[] args) {
+
+		MyATM m = new MyATM();
+		try {
+			m.m1();
+		} catch (Exception e) {
+			System.out.println("Exception caught in main");
+//			e.printStackTrace();
+		}
+
+	}
+
+}
